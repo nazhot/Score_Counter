fbed148a7d8da3bcbd18301011d8257209f2056c
nazhot <15216154+nazhot@users.noreply.github.com>
2023-05-19 14:29:18 -0600

    Setting up ScoreCard Skeleton
    Getting some styles set up, and organizing the overall layout of the
    component.

--- components/ScoreCard.js
+++ components/ScoreCard.js
@@ -1,24 +1,45 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { React } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
         borderRadius: "25%",
-        backgroundColor: "#3f1"
+        backgroundColor: "#3f1",
+    }, 
+    title: {
+        flex: 1,
+    },
+    scoreContainer: {
+        flexDirection: "row",
+        flex: 10,
+        color: "fff",
+    },
+    increment: {
+        flex: 1,
+        fontSize: 10,
+    },
+    score: {
+        flex: 9,
+        fontSize: 30,
     }
 });
 
 const ScoreCard = ( {player, score} ) => {
     return (
         <View style={styles.container}>
-            <View>
-                <Text>{player}</Text>
-                <Image></Image>
+            <View style={styles.title}>
+                <Text style={{flex: 1}}>{player}</Text>
+                <FontAwesome
+                    name="edit"
+                    size="30"
+                    color="fff"
+                />
             </View>
-            <View>
-                <Text>-</Text>
+            <View style={styles.scoreContainer}>
+                <Text style={styles.increment}>-</Text>
                 <Text>{score}</Text>
-                <Text>+</Text>
+                <Text style={styles.increment}>+</Text>
             </View>
         </View>
     );
