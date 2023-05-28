2112ced50795dda157984314fd29fece093fa248
nazhot <15216154+nazhot@users.noreply.github.com>
2023-05-28 15:24:28 -0600

    Added Skeletons for Settings/NewGame Screens
    Both are screens I want to incorporate for changing global/game
    settings, both literally do nothing, but the files have been created and
    they just return a view, and have been imported into the Navigator.

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ScoreScreen from './components/ScoreScreen';
 import PlayerScreen from './components/PlayerScreen';
+import NewGameScreen from './components/NewGameScreen';
+import SettingsScreen from './components/SettingsScreen';
 import { UserProvider } from './data/scoreData';
 
 const Stack = createNativeStackNavigator();
@@ -26,6 +28,14 @@ export default function App() {
               name="Player"
               component={PlayerScreen}
               options={ ( { route } ) => ( { title: route.params.name } ) }
+            />
+            <Stack.Screen
+              name="NewGame"
+              component={NewGameScreen}
+            />
+            <Stack.Screen
+              name="Settings"
+              component={SettingsScreen}
             />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ components/NewGameScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, TextInput } from "react-native";
+import InputWithLabel from "./InputWithLabel";
+import { useGlobalData, useGlobalDataDispatch } from "../data/globalData";
+
+
+const NewGameScreen = ( { navigation, routes } ) => {
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+export default NewGameScreen;
