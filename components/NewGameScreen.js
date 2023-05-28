034ea35b620d7c55996f790d5a9dd1bc28720159
nazhot <15216154+nazhot@users.noreply.github.com>
2023-05-28 15:51:16 -0600

    Adding to NewGame Screen
    Starting to add some code to get a basic view going, that will show you
    which game is currently selected.

--- components/NewGameScreen.js
+++ components/NewGameScreen.js
@@ -1,12 +1,39 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, ScrollView, Button } from "react-native";
 import InputWithLabel from "./InputWithLabel";
 import { useGlobalData, useGlobalDataDispatch } from "../data/globalData";
+import gameSettings from "../data/games";
+import { useState } from "react";
+
+
+function createGameSettingsComponents(game, setGame){
+    const gameButtons = [];
+    for ( const gameName in gameSettings) {
+        gameButtons.push(
+            <Button
+                onPress={() => setGame(gameName)}
+                title={gameName}
+                color={game === gameName ? "blue" : "white"}
+            />
+        );
+    }
+
+    return gameButtons;
+}
 
 
 const NewGameScreen = ( { navigation, routes } ) => {
+
+    const [game, setGame] = useState(Object.keys(gameSettings)[0]);
+    const currentGameSettings = gameSettings[game];
+
     return(
         <View>
+            <ScrollView>
+                {createGameSettingsComponents(game, setGame)}
+            </ScrollView>
+            <View>
 
+            </View>
         </View>
     );
 }
